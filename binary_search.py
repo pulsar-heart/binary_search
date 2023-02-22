aaa862af328bfc77f830bf618302b7d059eedcb8
Kaia Smith <coniferkai@gmail.com>
2023-02-21 22:06:38 -0800

    hopefully done lintering?

--- binary_search.py
+++ binary_search.py
@@ -12,7 +12,7 @@ def find_smallest_positive(xs):
     Assume that xs is a list of numbers sorted from LOWEST to HIGHEST.
     Find the index of the smallest positive number.
     If no such index exists, return `None`.
-    HINT: 
+    HINT:
     This is essentially the binary search algorithm from class,
     but you're always searching for 0.
     APPLICATION:
@@ -29,6 +29,7 @@ def find_smallest_positive(xs):
         return None
     if xs[-1] <= 0:
         return None
+
     def go(left, right):
         if left == right:
             if xs[left] > 0:
@@ -49,7 +50,7 @@ def count_repeats(xs, x):
     Assume that xs is a list of numbers sorted from HIGHEST to LOWEST,
     and that x is a number.
     Calculate the number of times that x occurs in xs.
-    HINT: 
+    HINT:
     Use the following three step procedure:
         1) use binary search to find the lowest index with a value >= x
         2) use binary search to find the lowest index with a value < x
@@ -69,7 +70,7 @@ def count_repeats(xs, x):
     '''
     if xs[1] == x:
         lo = 0
-    else: 
+    else:
         lo = highest_greater_index(xs, x)
         if lo is None:
             return 0
@@ -141,7 +142,7 @@ def highest_greater_index(xs, x):
         if xs[mid] <= x:
             right = mid - 1
         return go(left, right)
-  
+
     return go(0, len(xs) - 1)
 
 
@@ -157,7 +158,7 @@ def argmin(f, lo, hi, epsilon=1e-3):
         2) For each recursive call:
             a) select two points m1 and m2 that are between lo and hi
             b) one of the 4 points (lo,m1,m2,hi) must be the smallest;
-               depending on which one is the smallest, 
+               depending on which one is the smallest,
                you recursively call your function on the interval [lo,m2] or [m1,hi]
 
     APPLICATION:
